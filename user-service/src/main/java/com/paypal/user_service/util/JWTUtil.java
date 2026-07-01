8e972c6fda36309f5fbf96d643cc0bcf5c6f1ebf
Maithily-21 <maithilypatle@gmail.com>
2026-07-01 11:25:16 +0530

    Added jwt token utility and auth filter

--- user-service/src/main/java/com/paypal/user_service/util/JWTUtil.java
+++ user-service/src/main/java/com/paypal/user_service/util/JWTUtil.java
@@ -1,2 +1,56 @@
-package com.paypal.user_service.util;public class JWTUtil {
+package com.paypal.user_service.util;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+import java.security.Key;
+import java.util.Date;
+import java.util.Map;
+
+@Component
+public class JWTUtil {
+    //secret key must be of 32 bytes
+    private static final String SECRET = "secret123secret123secret123";
+
+    private Key getSignKey(){
+        return Keys.hmacShaKeyFor(SECRET.getBytes());
+    }
+    public String extractEmail(String token){
+        return Jwts.parser()
+                .setSigningKey(getSignKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    public boolean validateToken(String token , String username){
+        try{
+            extractEmail(token); //if parsing succeeds , token is valid
+            return true;
+        } catch (Exception e){
+            return false;
+        }
+    }
+
+
+    // Here we have assumed that username is same as email so we used same method
+    public String extractUsername(String token){
+        return Jwts.parser()
+                .setSigningKey(getSignKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    public String generateToken(Map<String,Object> claims , String email ){
+        return Jwts.builder().setClaims(claims)
+                .setSubject(email)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 86400000))
+                .signWith(getSignKey(), SignatureAlgorithm.HS256)
+                .compact(); // build
+    }
 }
--- user-service/src/main/java/com/paypal/user_service/util/JWTrequestFilter.java
+++ user-service/src/main/java/com/paypal/user_service/util/JWTrequestFilter.java
@@ -1,2 +1,38 @@
-package com.paypal.user_service.util;public class JWTrequestFilter {
+package com.paypal.user_service.util;
+
+import io.jsonwebtoken.io.IOException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+@Component
+public class JWTrequestFilter extends OncePerRequestFilter{
+    private final JWTUtil jwtUtil;
+    protected JWTrequestFilter(JWTUtil jwtUtil) {
+        this.jwtUtil = jwtUtil;
+    }
+    //filteration method , whenever a http request is called this method will invoke
+    @Override
+    protected void doFilterInternal(HttpServletRequest request , HttpServletResponse response , FilterChain chain) throws ServletException, IOException {
+        //getting authorization header from http request
+        final String authorizationHeader = request.getHeader("Authorization");
+        String username = null;
+        String jwt = null; // jwt token
+        //Checking if authorizationHeader starts with Bearer
+        if(authorizationHeader != null && authorizationHeader.startsWith("Bearer ")){
+            jwt = authorizationHeader.substring(7);
+            try{
+                username = jwtUtil.extractUsername(jwt);
+            } catch (Exception e){
+                //log exception
+            }
+        }
+        if(username != null && SecurityContextHolder.getContext().getAuthentication() == null){
+
+        }
+        
+    }
 }
